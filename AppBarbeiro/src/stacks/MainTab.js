b8ca2517de0817e9292264376d8ada40e090df12
Luiza <luikta@gmail.com>
2021-05-04 17:08:44 -0300

    TabBar and referents files (Home, Search, Appointments, Favorites and Profile) added

--- AppBarbeiro/src/stacks/MainTab.js
+++ AppBarbeiro/src/stacks/MainTab.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import CustomTabBar from '../components/CustomTabBar';
+
+import Home from '../screens/Home';
+import Search from '../screens/Search';
+import Appointments from '../screens/Appointments';
+import Favorites from '../screens/Favorites';
+import Profile from '../screens/Profile';
+
+
 const Tab = createBottomTabNavigator();
 
 export default () => (
-    <Tab.Navigator>
+    //passar o meu tabBar e não o padrão
+    <Tab.Navigator tabBar={props=><CustomTabBar {...props}/>}> 
         <Tab.Screen name="Home" component={Home}/>
         <Tab.Screen name="Search" component={Search}/>
         <Tab.Screen name="Appointments" component={Appointments}/>
